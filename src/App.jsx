3dde8f9c14ecaaa40089c522a45917e3886261e7
Martin Rouy <martinrouy03@gmail.com>
2024-03-26 17:53:09 +0100

    no component nor dynamic basket height

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import "./App.css";
 import "./responsive.css";
 import axios from "axios";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar, faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-library.add(faStar);
+library.add(faStar, faMinus, faPlus);
 import { useEffect, useState } from "react";
 
 function App() {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [basketContent, setBasketContent] = useState([]);
+  const [subTotal, setSubTotal] = useState(0);
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get("http://localhost:3200/");
@@ -18,7 +20,8 @@ function App() {
       setIsLoading(false);
     };
     fetchData();
-  }, []);
+  }, [setBasketContent]);
+
   const { categories, restaurant } = data;
   return isLoading ? (
     <span>En cours de téléchargement</span>
@@ -48,13 +51,43 @@ function App() {
           {categories.map((category, index) => {
             return (
               index < categories.length / 2 && (
-                <section>
+                // !(category.meals === {}}) && (
+                <section key={category.name}>
                   <h2>{category.name}</h2>
 
                   <div className="meals">
                     {category.meals.map((meal) => {
                       return (
-                        <div className="meal">
+                        <div
+                          key={meal.id}
+                          className="meal"
+                          onClick={() => {
+                            let isInBasket = false;
+                            const newBasketContent = [...basketContent];
+                            let newtotal = 0;
+                            newBasketContent.map((article) => {
+                              if (article.id.includes(meal.id)) {
+                                article.quantity = article.quantity + 1;
+                                isInBasket = true;
+                              }
+                              newtotal =
+                                newtotal + article.quantity * article.price;
+                            });
+                            if (!isInBasket) {
+                              const newMeal = {
+                                title: meal.title,
+                                quantity: 1,
+                                price: meal.price,
+                                id: meal.id,
+                              };
+                              newtotal =
+                                newtotal + newMeal.quantity * newMeal.price;
+                              newBasketContent.push(newMeal);
+                            }
+                            setSubTotal(newtotal);
+                            setBasketContent(newBasketContent);
+                          }}
+                        >
                           <div className="meal-desc">
                             <h3>{meal.title}</h3>
                             <p>{meal.description}</p>
@@ -92,11 +125,98 @@ function App() {
             );
           })}
         </div>
-        <div className="panier">
-          <button>Valider mon panier</button>
-          <div className="panier-content">
+        <div
+          className="panier"
+          // style={{ height: basketContent.length === 0 ? "150px" : "auto" }}
+        >
+          <button
+            style={{
+              backgroundColor:
+                basketContent.length === 0 ? "#bac3c3" : "#00CCBC",
+              color: basketContent.length === 0 ? "#868a8a" : "#ffffff",
+            }}
+          >
+            Valider mon panier
+          </button>
+          {basketContent.length === 0 ? (
             <p>Votre panier est vide</p>
-          </div>
+          ) : (
+            <div>
+              <div className="panier-content">
+                {basketContent.map(({ title, quantity, price, id }, index) => {
+                  return (
+                    <div className="panier-article" key={id}>
+                      <div className="quantity">
+                        <div
+                          className="qtityButton"
+                          onClick={() => {
+                            const newBasketContent = [...basketContent];
+                            newBasketContent[index].quantity = quantity - 1;
+
+                            let newtotal = 0;
+                            newBasketContent.map((article) => {
+                              newtotal =
+                                newtotal + article.quantity * article.price;
+                            });
+                            setSubTotal(newtotal);
+                            if (newBasketContent[index].quantity === 0) {
+                              newBasketContent.splice(index, 1);
+                            }
+                            setBasketContent(newBasketContent);
+                          }}
+                        >
+                          <FontAwesomeIcon
+                            icon="fa-solid fa-minus"
+                            size="2xs"
+                            style={{ color: "#00ccbc" }}
+                          />
+                        </div>
+                        <span>{quantity}</span>
+                        <div
+                          className="qtityButton"
+                          onClick={() => {
+                            const newBasketContent = [...basketContent];
+                            newBasketContent[index].quantity = quantity + 1;
+                            let newtotal = 0;
+                            newBasketContent.map((article) => {
+                              newtotal =
+                                newtotal + article.quantity * article.price;
+                            });
+                            setSubTotal(newtotal);
+                            setBasketContent(newBasketContent);
+                          }}
+                        >
+                          <FontAwesomeIcon
+                            icon="fa-solid fa-plus"
+                            size="2xs"
+                            style={{ color: "#00ccbc" }}
+                          />
+                        </div>
+                      </div>
+                      <div className="order-details">
+                        <p>{title}</p>
+                        <span>{(quantity * price).toFixed(2)} €</span>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="panier-bottom">
+                <div className="panier-bottom-line">
+                  <p>Sous-total</p>
+                  <span>{subTotal.toFixed(2)} €</span>
+                </div>
+                <div className="panier-bottom-line">
+                  <p>Frais de livraison</p>
+                  <span>2.5 €</span>
+                </div>
+                <div className="panier-bottom-line">
+                  <p>Total</p>
+                  <span>{(subTotal + 2.5).toFixed(2)} €</span>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </main>
     </>
